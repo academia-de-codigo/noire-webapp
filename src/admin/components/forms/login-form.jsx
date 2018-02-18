485ff57a95be83e098052ff2eaf0bbd4090c9d9d
Rui Ferrão <mail@ruiferrao.com>
2018-02-18 14:20:03 +0000

    get rid of complex relative paths in import statements

---
+++ src/admin/components/forms/login-form.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    state = {};
+
+    render() {
+        return <div>Login Form</div>;
+    }
+}
+
+export default LoginForm;
--- src/admin/components/pages/login-page.jsx
+++ src/admin/components/pages/login-page.jsx
@@ -1,9 +1,10 @@
-// @flow
 import React from 'react';
+import LoginForm from 'admin/components/forms/login-form';
 
 const LoginPage = () => (
     <div className="ui container">
         <h1>Login Page</h1>
+        <LoginForm />
     </div>
 );
 
