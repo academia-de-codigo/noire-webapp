deb99dde299643ed1df19b7b3b5c276769008db9
Rui Ferrão <mail@ruiferrao.com>
2018-03-16 20:14:00 +0000

    added redux logger

--- src/index.jsx
+++ src/index.jsx
@@ -4,24 +4,28 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunk from 'redux-thunk';
-import 'semantic-ui-css/semantic.min.css';
+import logger from 'redux-logger';
 import App from 'app/app';
 import rootReducer from 'reducer';
 import * as action from 'auth/auth-actions';
+import 'semantic-ui-css/semantic.min.css';
 import 'app/app.css';
 
-let reduxMiddleware = applyMiddleware(thunk);
+const reduxMiddlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
     // eslint-disable-next-line no-console
-    console.log('Running in development mode!');
+    console.log(`Running in Development @ ${new Date()} `);
 
-    reduxMiddleware = composeWithDevTools(reduxMiddleware);
+    reduxMiddlewares.push(logger);
 }
 
-const store = createStore(rootReducer, reduxMiddleware);
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(...reduxMiddlewares))
+);
 
 if (localStorage.jwt) {
     store.dispatch(action.login(localStorage.jwt));
--- src/pages/admin/admin-page.jsx
+++ src/pages/admin/admin-page.jsx
@@ -6,8 +6,8 @@ function AdminPage() {
     const links = [
         { id: 0, path: '/', text: 'Home' },
         { id: 1, path: '/admin', text: 'Admin' },
-        { id: 1, path: '/admin-route1', text: 'Admin Route 1' },
-        { id: 2, path: '/admin-route2', text: 'Admin Route 2' }
+        { id: 2, path: '/admin-route1', text: 'Admin Route 1' },
+        { id: 3, path: '/admin-route2', text: 'Admin Route 2' }
     ];
     return (
         <div className="page">
